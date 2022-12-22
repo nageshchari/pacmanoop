8f3c1f0bcdf8d612e2b9633b47f017c25f2507ce
user <user@LAPTOP-TANEP423>
2022-12-22 17:04:01 +0530

    helper class

---
+++ Pacmangame/src/pacman/Helper.java
@@ -0,0 +1,22 @@
+package pacman;
+
+public class Helper {
+	private Integer score=0;
+	private Integer MAX=15;
+
+	
+	public Integer getMAX() {
+		return MAX;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+	
+}
+
+}
