bad6a63a3e1330ba2a1e7bf3a49433b5117f4f55
shubham <user@192.168.1.5>
2022-12-23 08:56:10 +0530

    obstacle

--- Pacmangame/src/pacman/pacManGame.java
+++ Pacmangame/src/pacman/pacManGame.java
@@ -46,6 +46,8 @@ public class pacManGame {
 	        	map[i][j] = "#";
 	        }else if(j == 15 && i > 10 && i < 16) {
 	        	map[i][j]= "#";
+	        }else if(i ==2 && j > 8 && j<16) {
+	        	map[i][j] = "#";
 	        }else {
 	          map[12][12] = "0";
 	          map[i][j] = ".";
